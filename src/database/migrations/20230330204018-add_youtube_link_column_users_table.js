bf4ec2b219d28023e42b1520f1186e0c11d0ddb6
Dirk Gaston <dirkgaston87@gmail.com>
2023-03-30 19:08:19 -0300

    adjust user update endpoint

--- src/database/migrations/20230330204018-add_youtube_link_column_users_table.js
+++ src/database/migrations/20230330204018-add_youtube_link_column_users_table.js
@@ -10,6 +10,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('users', 'youtube_link');
-
+    await queryInterface.removeColumn("users", "youtube_link");
+  },
 };
